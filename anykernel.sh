f6ce149c4eab739f46ecc928d3d0638dbf2f0bb2
babayybay <147687642+babay162@users.noreply.github.com>
2025-02-02 21:29:40 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NAPI-overissue by babay and Re-Noroi
+kernel.string=NAPI-overissue by Babay, Re-Noroi & Emanuel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
